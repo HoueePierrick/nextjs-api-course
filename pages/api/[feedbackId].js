dbb9c044e3d8a3bfb56a71d2adf63dbcd50dc277
PierrickHOUEE <houee.pierrick@gmail.com>
2023-04-09 16:20:18 +0200

    Finished section

---
+++ pages/api/[feedbackId].js
@@ -0,0 +1,15 @@
+// Dynamic API page
+import { buildFeedbackPath, extractFeedback } from "./feedback";
+
+function handler(req, res) {
+  // To retrieve the dynamic feedback ID
+  const feedbackId = req.query.feedbackId;
+  const filePath = buildFeedbackPath();
+  const feedbackData = extractFeedback(filePath);
+  const selectedFeedback = feedbackData.find(
+    (feedback) => feedback.id === feedbackId
+  );
+  res.status(200).json({ feedback: selectedFeedback });
+}
+
+export default handler;
--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,18 +1,44 @@
+import { Fragment, useState } from "react";
 import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 
 function feedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
   return (
-    <ul>
-      {props.feedBackItems.map((item) => {
-        return <li id={item.id}>{item.text}</li>;
-      })}
-    </ul>
+    <Fragment>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => {
+          return (
+            <li id={item.id}>
+              {item.text}
+              <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+                Show details
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </Fragment>
   );
 }
 
 // Prerendering
 export async function getStaticProps() {
   //   Never use fetch() to call own API in getStaticProps
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
 }
 
 export default feedbackPage;
